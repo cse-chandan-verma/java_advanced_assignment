8b7e0866d968200b080048c838c594030e7c0bc9
cse-chandan-verma <cse.chandan.verma@gmail.com>
2026-02-25 15:20:15 +0530

    CRUD Operations in Spring MVC

---
+++ demo_SpringMVC/src/main/java/com/springmvc/demo/service/EmployeeService.java
@@ -0,0 +1,30 @@
+package com.springmvc.demo.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.springmvc.demo.model.Employee;
+import com.springmvc.demo.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	public EmployeeRepository empRepository;
+	
+	public EmployeeService(EmployeeRepository empRepository) {
+		this.empRepository = empRepository;
+	}
+	
+	public List<Employee> getAllEmployees(){
+		return empRepository.findAll();
+	}
+
+	public void saveEmployee(Employee employee) {
+		empRepository.save(employee);
+	}
+	
+	public void deleteEmployee(Long id) {
+		empRepository.deleteById(id);
+	}
+}
+
+
